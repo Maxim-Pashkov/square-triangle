cbc3fa989cad1a2586a1b4fdc7aed90037a833ab
Maxim Pashkov <max.pashkov.rez@yandex.ru>
2020-01-08 12:30:45 +0400

    added calc square

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log(1);
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+document.querySelector('form').addEventListener('submit', e => {
+    e.preventDefault();
+    const {target: form} = event;
+    const a = +form.elements.a.value;
+    const h = +form.elements.h.value;
+
+    const square = 0.5 * a * h;
+
+    if(!a || !h) {
+        document.getElementById('output').style.display = 'none';
+    } else {
+        document.getElementById('output').style.display = '';
+        document.getElementById('output-square').textContent = +(Math.round(square + "e+2")  + "e-2");;
+    }   
+});
