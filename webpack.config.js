c792e965fb49977ae8400fd864071781fe3ad763
Maxim Pashkov <max.pashkov.rez@yandex.ru>
2020-01-09 21:12:50 +0400

    revert prod config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,17 @@
 const path = require('path');
 
-module.exports = env => {
-  if(env) {
-    console.log('NODE_ENV: ', env.NODE_ENV);
-    console.log('Production: ', env.production);
-  }
-
-  return {
-    entry: './src/index.js',
-    output: {
-      filename: 'index.js',
-      path: path.resolve(__dirname, 'public'),
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
-      ],
-    },
-  }; 
-};
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+}; 
