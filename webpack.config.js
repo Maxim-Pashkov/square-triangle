13bc14a73f902b64a99def2cc0d32f052895315c
Maxim Pashkov <max.pashkov.rez@yandex.ru>
2020-01-09 21:02:00 +0400

    logged node_env

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,24 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'public'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
+module.exports = env => {
+  if(env) {
+    console.log('NODE_ENV: ', env.NODE_ENV);
+    console.log('Production: ', env.production);
+  }
 
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+  }; 
 };
